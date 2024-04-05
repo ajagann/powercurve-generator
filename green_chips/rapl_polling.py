e6e6a27187901eba4e835807ba402b847c2f09bd
Danilo Oliveira <danilo.oliveira@intapp.com>
2024-04-05 12:03:42 -0300

    fixed rapl_polling

--- green_chips/rapl_polling.py
+++ green_chips/rapl_polling.py
@@ -7,31 +7,31 @@ import pyRAPL
 pyRAPL.setup(devices=[pyRAPL.Device.PKG]) 
 # pyRAPL.setup()
 
-csv_output = pyRAPL.outputs.CSVOutput('polling-results.csv')
-@pyRAPL.measureit(output=csv_output)   
-
 class RAPLPolling():
-    def run_get_cpu(self):
+    csv_output = pyRAPL.outputs.CSVOutput('polling-results.csv')
+    @pyRAPL.measureit(output=csv_output)   
+    
+    def run_get_cpu():
         return psutil.cpu_percent(interval=1, percpu=True), psutil.cpu_freq().current
     
-    def run_polling(self):
-        data_points = []
-        start_time = time.time()
-        print("Start time:",start_time)
+    data_points = []
+    start_time = time.time()
+    print("Start time:", start_time)
 
-        for _ in range(5):  # number of process
-            meter = pyRAPL.Measurement(label='polling')
-            meter.begin()
+    for _ in range(5):   # number of process
+        meter = pyRAPL.Measurement(label='polling')
+        meter.begin()
 
-            utilization, frequency = self.run_get_cpu()
-            
-            meter.end()
-            data_points.append((utilization, frequency))
-            time.sleep(1)
-            # print(data_points)
+        utilization, frequency = run_get_cpu()
 
-        end_time = time.time()
-        print("End time:",end_time)
+        meter.end()
+        data_points.append((utilization, frequency))
+        time.sleep(1)
+
+    end_time = time.time()
+    print("End time:", end_time)
+
+    csv_output.save()
     
 if __name__ == "__main__":
     logging.basicConfig()
@@ -41,6 +41,3 @@ if __name__ == "__main__":
     logging.getLogger().setLevel(logging.DEBUG)
 
     poll = RAPLPolling()
-    poll.run_polling()
-
-csv_output.save()
